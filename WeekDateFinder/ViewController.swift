d1b9497d082718350db09110c2c59ebb87521a02
Bolshakov Nikita <37251655+nikita2717@users.noreply.github.com>
2020-05-02 21:10:56 +0300

    added Outlets

--- WeekDateFinder/ViewController.swift
+++ WeekDateFinder/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
     }
-
-
+    
+    
 }
 
